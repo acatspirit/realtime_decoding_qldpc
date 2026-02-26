8252143d92acb017d9c9d02844ca38560063cf5a
acatspirit <acatspirit@gmail.com>
2026-02-26 16:21:28 -0500

    have to increase the memory again

--- simulation_scripts/slurm_threshold_sweep.sh
+++ simulation_scripts/slurm_threshold_sweep.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/threshold_%A_%a.out
 #SBATCH --error=logs/threshold_%A_%a.err
 #SBATCH --time=24:00:00
-#SBATCH --mem=4G
+#SBATCH --mem=10G
 #SBATCH --cpus-per-task=4
 #SBATCH --array=0-39
 

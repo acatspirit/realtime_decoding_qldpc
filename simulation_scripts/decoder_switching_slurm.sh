09815df92be623d1891afc6808cf52a4fae36e79
acatspirit <acatspirit@gmail.com>
2026-05-02 12:13:44 -0400

    wrong file name

--- simulation_scripts/decoder_switching_slurm.sh
+++ simulation_scripts/decoder_switching_slurm.sh
@@ -3,9 +3,9 @@
 #SBATCH --array=0-149
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
-#SBATCH --time=24:00:00
+#SBATCH --time=8:00:00
 #SBATCH --output=logs/%a.out
 
 # Activate your environment
 conda activate realtime_decoding
-python cluster_simulation.py
+python decoder_switching.py

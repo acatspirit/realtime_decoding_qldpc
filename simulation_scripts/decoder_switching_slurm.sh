612a8944d6d8926021e6bb3bbbd35017ef8ac0e2
acatspirit <acatspirit@gmail.com>
2026-05-06 14:35:18 -0400

    try deleting #SBATCH --cpus-per-task=1

--- simulation_scripts/decoder_switching_slurm.sh
+++ simulation_scripts/decoder_switching_slurm.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=qec_sim
 #SBATCH --array=0-6332%500
-#SBATCH --cpus-per-task=1
 #SBATCH --mem=15G
-#SBATCH --time=24:00:00
+#SBATCH --time=8:00:00
 #SBATCH --output=logs/%a.out
 
 # Activate your environment

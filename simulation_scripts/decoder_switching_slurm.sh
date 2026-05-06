86bedba2589ea892821278af2d455b62620680d8
acatspirit <acatspirit@gmail.com>
2026-05-06 14:46:28 -0400

    I have to incrementally submit to the cluster.. annoying. 0-2k

--- simulation_scripts/decoder_switching_slurm.sh
+++ simulation_scripts/decoder_switching_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=qec_sim
-#SBATCH --array=3001-5000 # total 6332
-#SBATCH --mem=15G
+#SBATCH --array=0-2000 # total 6332
+#SBATCH --mem=20G
 #SBATCH --time=8:00:00
 #SBATCH --output=logs/%a.out
 

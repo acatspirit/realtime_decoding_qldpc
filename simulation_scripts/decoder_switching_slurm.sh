d3b1f7517415f953d3b65186220eefa6a3b332d5
acatspirit <acatspirit@gmail.com>
2026-05-06 14:42:24 -0400

    try job 1001-3000

--- simulation_scripts/decoder_switching_slurm.sh
+++ simulation_scripts/decoder_switching_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=qec_sim
-#SBATCH --array=0-1000 # total 6332
+#SBATCH --array=1001-3000 # total 6332
 #SBATCH --mem=15G
 #SBATCH --time=8:00:00
 #SBATCH --output=logs/%a.out

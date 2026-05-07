ac95aeeaec98dfd2e45754a878fc746d331a647d
acatspirit <acatspirit@gmail.com>
2026-05-07 12:40:00 -0400

    run for relay now

--- simulation_scripts/decoder_switching_slurm.sh
+++ simulation_scripts/decoder_switching_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=bb_switching
-#SBATCH --array=0-31664 # total 6332
+#SBATCH --array=0-6332 # total 6332
 #SBATCH --mem=20G
 #SBATCH --time=8:00:00
 #SBATCH --output=logs/%a.out

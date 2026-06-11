dd25c5362b5b4290c410d04b26f04ec6e2c3b9a8
acatspirit <acatspirit@gmail.com>
2026-06-11 13:20:08 -0400

    fixed the error with the infinite loop for the UF decoder without cluster sizes

--- src/realtime_decoding/union_find.py
+++ src/realtime_decoding/union_find.py
@@ -9,7 +9,7 @@ from ldpc_post_selection.decoder import SoftOutputsBpLsdDecoder
 from ldpc_post_selection.cluster_tools import compute_cluster_norm_fraction
 # try UF from the other repo now too 
 import py_wrapper.py_decoder as uf
-from py_wrapper.some_codes import surface_code_non_periodic
+from py_wrapper.some_codes import surface_code_non_periodic, get_bb
 from quits.qldpc_code import HgpCode
 import ldpc.codes as codes 
 from realtime_decoding.decoder_switching import get_BB_circuit
@@ -31,6 +31,15 @@ def get_bb_code_parity_and_logs(d):
         B_y_pows=code_params['B_y_pows'],
     )
     return bb.hx, bb.lx
+
+def get_bb_codes_uf_decoder(d):
+    l_dims = [[6, 6], [15, 3], [9, 6], [12, 6], [12, 12]]
+    pow_a = [[3, 1, 2], [9, 1, 2], [3, 1, 2], [3, 1, 2], [3, 2, 7]]
+    pow_b = [[3, 1, 2], [0, 2, 7], [3, 1, 2], [3, 1, 2], [3, 1, 2]]
+
+    
+    Hx, Hz = get_bb(l_dims[0][0], l_dims[0][1], pow_a[0], pow_b[0])  #.astype(np.uint8)
+    return Hx, Hz
 np.set_printoptions(threshold=sys.maxsize)
 
 def get_cluster_norm(cluster_sizes, order=2, type="LSD"):
@@ -46,40 +55,43 @@ d = 6
 
 # H, L = surface_code_non_periodic(7)
 H,L = get_bb_code_parity_and_logs(d=d)
+# H,Hz = get_bb_codes_uf_decoder(d)
 # print(f"H:\n{[(H.nonzero()[0][i],H.nonzero()[1][i]) for i in range(len(H.nonzero()[0]))]}")
 # print(f"L:\n{L.nonzero()}")
 # H = ldpc.codes.hamming_code(5)
 p = 0.1
 
-decoder_uf = uf.UFDecoder(H)
+decoder_uf = uf.UFDecoder(H.astype(np.uint8))
 decoder_lsd = SoftOutputsBpLsdDecoder(H, p=p*np.ones(H.shape[1]), bp_method="minimum_sum", max_iter=20, schedule="serial", osd_method="osd_cs", osd_order=0)
 
 # syndrome = np.random.randint(size=H.shape[0], low=0, high=2).astype(np.uint8)
-syndrome = np.random.binomial(n=1, p=p, size=H.shape[0]).astype(np.uint8)
+error = np.random.binomial(n=1, p=p, size=H.shape[1]).astype(np.uint8)
+syndrome = (H @ error % 2).astype(np.uint8)
 erasures = np.zeros(shape=H.shape[1], dtype=np.uint8)
 print(f"Syndrome: {syndrome}")
-# decoding = union_find.decode(syndrome)
-# print(f"Decoding: {decoding}")
+# print(f"H:\n{H}")
+decoder_uf.ldpc_decode(syndrome, erasures)
+print(f"Decoding: {decoder_uf.correction}")
 # decoding_syndrome = H @ decoding % 2
 # print(f"Decoding syndrome: {decoding_syndrome}")
 
 
 # test cluster sizes
-found_cluster_sizes_uf = decoder_uf.ldpc_decode(syndrome, erasures)
-num_qubits = H.shape[1]
-clusters_including_outside_uf = np.append(found_cluster_sizes_uf[0], num_qubits - np.sum(found_cluster_sizes_uf[0])) # this includes the outside cluster, which is the largest one
-correction_uf = decoder_uf.correction
-cluster_norm_uf = get_cluster_norm(clusters_including_outside_uf, order=2, type="UF")
-print(f"Found cluster sizes: {clusters_including_outside_uf}")
-print(f"Cluster norm for uf: {cluster_norm_uf}")
-print("\n--------------------\n")
-correction_lsd, correction_bp, converge, soft_outputs_lsd = decoder_lsd.decode(syndrome)
-# print(f"Correction for lsd: {correction_lsd}")
-cluster_sizes = np.delete(soft_outputs_lsd['cluster_sizes'], np.argmax(soft_outputs_lsd['cluster_sizes']))
-cluster_norm_lsd = get_cluster_norm(soft_outputs_lsd['cluster_sizes'], order=2, type="LSD")
- # remove the 0 cluster size (for unclustered qubits)
-print(f"cluster sizes(qubits) for lsd: {soft_outputs_lsd['cluster_sizes']}")
-print(f"Cluster norm for lsd: {cluster_norm_lsd}")
+# found_cluster_sizes_uf = decoder_uf.ldpc_decode(syndrome, erasures)
+# num_qubits = H.shape[1]
+# clusters_including_outside_uf = np.append(found_cluster_sizes_uf[0], num_qubits - np.sum(found_cluster_sizes_uf[0])) # this includes the outside cluster, which is the largest one
+# correction_uf = decoder_uf.correction
+# cluster_norm_uf = get_cluster_norm(clusters_including_outside_uf, order=2, type="UF")
+# print(f"Found cluster sizes: {clusters_including_outside_uf}")
+# print(f"Cluster norm for uf: {cluster_norm_uf}")
+# print("\n--------------------\n")
+# correction_lsd, correction_bp, converge, soft_outputs_lsd = decoder_lsd.decode(syndrome)
+# # print(f"Correction for lsd: {correction_lsd}")
+# cluster_sizes = np.delete(soft_outputs_lsd['cluster_sizes'], np.argmax(soft_outputs_lsd['cluster_sizes']))
+# cluster_norm_lsd = get_cluster_norm(soft_outputs_lsd['cluster_sizes'], order=2, type="LSD")
+#  # remove the 0 cluster size (for unclustered qubits)
+# print(f"cluster sizes(qubits) for lsd: {soft_outputs_lsd['cluster_sizes']}")
+# print(f"Cluster norm for lsd: {cluster_norm_lsd}")
 
 
 # # 2. Simulate an actual physical error pattern (e.g., flip 3 random qubits)

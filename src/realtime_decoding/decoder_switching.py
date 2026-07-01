checking whether there is something wrong with my cluster norm calculation by using the built in

src/realtime_decoding/decoder_switching.py
@@ -37,13 +37,18 @@ def get_cluster_norm(cluster_sizes, num_qubits,order=2, type="LSD"):
         return compute_cluster_norm_fraction(cluster_sizes, order=order) # this should include the largest cluster - i.e. whichever one doesn't have errors
     else: # using UF decoder
         if sum(cluster_sizes) == num_qubits:
-            # clusters_including_outside_uf = cluster_sizes
-            cluster_sizes_inside = cluster_sizes[cluster_sizes != np.argmax(cluster_sizes)]
+            cluster_norm = compute_cluster_norm_fraction(np.sort(cluster_sizes)[::-1], order=order) # make sure to sort in descending order
         else:
-            # clusters_including_outside_uf = np.append(cluster_sizes, num_qubits - np.sum(cluster_sizes))
-            cluster_sizes_inside = cluster_sizes
-        cluster_powers = np.power(cluster_sizes_inside, order)
-        cluster_norm = np.sum(cluster_powers)**(1/order) / num_qubits
+            cluster_sizes_all_clusters = np.append(cluster_sizes, num_qubits - np.sum(cluster_sizes))
+            cluster_norm = compute_cluster_norm_fraction(np.sort(cluster_sizes_all_clusters)[::-1], order=order)
+        # if sum(cluster_sizes) == num_qubits:
+        #     # clusters_including_outside_uf = cluster_sizes
+        #     cluster_sizes_inside = cluster_sizes[cluster_sizes != np.argmax(cluster_sizes)]
+        # else:
+        #     # clusters_including_outside_uf = np.append(cluster_sizes, num_qubits - np.sum(cluster_sizes))
+        #     cluster_sizes_inside = cluster_sizes
+        # cluster_powers = np.power(cluster_sizes_inside, order)
+        # cluster_norm = np.sum(cluster_powers)**(1/order) / num_qubits
         return cluster_norm
 
 ###########

src/realtime_decoding/decoding.py
@@ -246,10 +246,11 @@ class UnionFindWrapper:
         self.cluster_sizes = found_cluster_sizes
         self.cluster_map = cluster_map
         self.committed_clusters = np.where(self.commit_region, self.cluster_map, 0)
+        self.committed_cluster_sizes = found_cluster_sizes
         
         if np.all(self.committed_clusters != self.cluster_map):
             cluster_ids, sizes = np.unique(self.committed_clusters, return_counts=True)
-            self.cluster_sizes = sizes
+            self.committed_cluster_sizes = sizes
         # 5. Extract the actual correction pattern produced by the decode logic
         correction = self.decoder.correction
         
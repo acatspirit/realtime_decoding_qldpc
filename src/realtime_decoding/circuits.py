c201a0eecd76d40db262e603ff607aa841fc9498
Evangelia Takou <evatakou@Evangelias-MacBook-Pro.local>
2026-07-02 20:43:45 -0400

    updated leakage sim

--- src/realtime_decoding/circuits.py
+++ src/realtime_decoding/circuits.py
@@ -236,7 +236,7 @@ def create_bb_codes_circuit_ionic_model(code_name: str, p: float, num_rounds: in
     return labeled_circuit, bb
 
 
-def add_independent_leakage_errors_per_round(circuit: stim.Circuit, p_leak = 1e-5):
+def add_independent_leakage_errors_per_round(circuit: stim.Circuit, n: int, p_leak = 1e-5):
     '''Add independent leakage errors on the qubits, assuming a fixed leakage error rate per round of p_leak.
        Note, leakage errors are put at the beginning of each round: 
        -after instruction "R" for first round,
@@ -245,6 +245,7 @@ def add_independent_leakage_errors_per_round(circuit: stim.Circuit, p_leak = 1e-
 
     Inputs:
         circuit: the stim circuit which could have Pauli errors
+        n: # of data qubits for the bb code
         p_leak:  the leakage rate (default is chosen for trapped ions based on https://arxiv.org/pdf/2604.19481)
     Outputs:
         new_circuit: a deltakit_stim circuit which includes also leakage errors (leakage errors are also depicted in rates of dem)
@@ -252,18 +253,26 @@ def add_independent_leakage_errors_per_round(circuit: stim.Circuit, p_leak = 1e-
 
     NOTE: IF leakage is combined with loss, we need to be extra careful, cause we might not be able to sample independently erasures and loss.
           For example, if a leaked qubit interacts with lost qubit, the loss propagates to the leaked qubit.
-
     
     TODO: Think more how we can combine loss + leakage + Pauli errors. Or we will study loss and leakage as independent mechanisms.
 
-
+    NOTE: This will only be used to sample det events, but we will use the leakage-unaware dem, + restrict det events only on regular detectors.
+          In this way, we contain the leakage features, but we do not exploit whether or not there was leakage.
+          We could do some post-selection thing.
     '''
 
     new_circuit = deltakit_stim.Circuit()
 
     targets = [stim.GateTarget(k) for k in range(circuit.num_qubits)]  #These are all the qubits in the circuit
 
-    for inst in circuit:
+    max_det= circuit.num_detectors-1 #max detector in original circuit --- we will append leakage-herald detectors starting from max_det+1 as a coordinate
+
+    num_recs_any_but_last = n
+
+    time_slice=0
+    cnt=max_det+1
+    
+    for inst in circuit.flattened():
 
         if inst.name == "MR": #intermediate measurements of ancilla
 
@@ -271,17 +280,202 @@ def add_independent_leakage_errors_per_round(circuit: stim.Circuit, p_leak = 1e-
             new_circuit.append(name=inst.name,targets=inst.targets_copy(),arg=inst.gate_args_copy()) #Apply MR
             new_circuit.append(name="LEAKAGE",targets=targets,arg=p_leak)                            #Leak again ALL QUBITS in the beginning of next round
 
+
+            #----Append the leakage-herald detectors (first coord index > max_detector in regular circuit)
+
+            
+            if time_slice==0: #for the time_slice=0 ignore half of the detectors, probably outcomes are non-deterministic...
+
+                for k in range(num_recs_any_but_last//2, num_recs_any_but_last):
+                    new_circuit.append("DETECTOR",stim.target_rec(-num_recs_any_but_last-k-1),(cnt,time_slice))
+                    cnt+=1
+            else:
+                for k in range(num_recs_any_but_last):
+                    new_circuit.append("DETECTOR",stim.target_rec(-num_recs_any_but_last-k-1),(cnt,time_slice))
+                    cnt+=1
+
+            time_slice+=1
+
+            
         elif inst.name=="R": #Reset of all qubits @ beginning of experiment
             
+              
             new_circuit.append(name=inst.name,targets=inst.targets_copy(),arg=inst.gate_args_copy()) #Apply R
             new_circuit.append(name="LEAKAGE",targets=inst.targets_copy(),arg=p_leak)                #Leak qubits 
-            
+
 
         elif inst.name=="M": #final data qubit measurements 
             new_circuit.append(name="HERALD_LEAKAGE_EVENT",targets=inst.targets_copy())               #Herald the leakage on data qubit measurements
             new_circuit.append(name=inst.name,targets=inst.targets_copy(),arg=inst.gate_args_copy())  #Measure the data qubits
-        
+
+            #Add again the leakage detectors
+
+            for k in range(num_recs_any_but_last):
+                new_circuit.append("DETECTOR",stim.target_rec(-num_recs_any_but_last-k-1),(cnt,time_slice))
+                cnt+=1            
+
+
         else: #any other instruction
             new_circuit.append(name=inst.name,targets=inst.targets_copy(),arg=inst.gate_args_copy())  #All other instructions remain invariant
 
-    return new_circuit
+        
+    #Need to update the regular dets for time-slice>1
+
+    final_circuit = deltakit_stim.Circuit()
+
+    regular_dets = []
+    leakage_dets = []
+
+    DET_CNT = 0
+
+    #------- Now we need to fix the regular detectors definitions while preserving the leakage aware ones ---------------------------------------------------------
+
+    for k in range(len(new_circuit)):
+        
+        inst = new_circuit[k]
+
+        if inst.name=="DETECTOR":
+            
+            coords = inst.gate_args_copy() #detectors w/ one entry are the regular detectors, not the leakage aware ones
+            targets = inst.targets_copy()
+
+            if len(coords)==1: #The detectors we need to fix --- ALL OF THEM ARE REGULAR DETS
+
+                
+                if coords[0]==0:  #First rd dets are fine
+                    final_circuit.append(inst)
+                    regular_dets.append(DET_CNT)
+                    DET_CNT+=1
+                    continue
+                     
+                if len(targets)==2: #Bulk dets
+
+                    val_to_shift = targets[1].value
+                    new_targets = [targets[0],stim.target_rec(val_to_shift-num_recs_any_but_last)]
+
+                if len(targets)>2: #Last round dets from final data measurements (last record is 18 values)
+                    
+                    vals = []
+
+                    for k in range(len(targets)): 
+                        vals.append(targets[k].value)
+
+                    loc = vals.index(min(vals)) #loc of detector (most negative detector needs to be shifted again)
+                    
+                    new_targets = targets 
+                    val_to_shift = vals[loc]
+                    new_targets[loc] = stim.target_rec(val_to_shift-num_recs_any_but_last)
+
+                final_circuit.append("DETECTOR",new_targets,coords)
+
+                regular_dets.append(DET_CNT)
+
+            else: #Those are leakage dets
+                final_circuit.append(inst)
+                leakage_dets.append(DET_CNT)
+            
+            
+            DET_CNT+=1
+
+
+        else:
+            final_circuit.append(inst)
+
+    #I think this circuit is now correct.
+    #Now, output also the names of dets which are leakage detectors or regular detectors.
+
+    det_types = {"regular_dets": regular_dets, "leakage_dets": leakage_dets}
+
+
+    return final_circuit,det_types
+
+#TODO: Do more testing -- This is unused for now
+def drop_leakage_dets(circuit: deltakit_stim.Circuit, det_types: dict):
+    '''This drops the leakage dets. Tested on a circuit, seems to produce the regular dem.'''
+    dem_deltakit = circuit.detector_error_model(flatten_loops=True) #note this is not stim.DetectorErrorModel 
+    dem = stim.DetectorErrorModel(str(dem_deltakit))   #convert it to stim.DetectorErrorModel
+
+    #now drop detectors which correspond to the det_types['leakage_dets']
+    print("leakage dets:",det_types['leakage_dets'])
+
+    leakage_dets = det_types['leakage_dets']
+
+    # print(dem)
+    new_DEM = stim.DetectorErrorModel()
+    for inst in dem:
+
+        if inst.type=="error":
+
+            flag_in_leakage = False
+            targets = inst.targets_copy()
+
+            dets = [t.val for t in targets if t.is_relative_detector_id()]
+
+
+            for det in dets:
+                if det in leakage_dets:
+                    flag_in_leakage=True
+                    break 
+
+            if not flag_in_leakage:
+                new_DEM.append(inst)
+            
+        else:  #append only dets which are not leakage dets, i.e., they have len(coords)=1
+            
+            if inst.type=="detector":
+
+                # det_neam = inst.targets_copy()
+                coords=inst.args_copy()
+                if len(coords)>1:
+                    continue 
+                else:
+                    new_DEM.append(inst)
+                
+    #Rename also the dets so that we start from regular 0,1,... etc
+    final_DEM = stim.DetectorErrorModel()
+
+
+    mapping = {}
+
+    next_id = 0
+    for inst in new_DEM:
+        if inst.type == "detector":
+            det = inst.targets_copy()[0].val
+            mapping[det] = next_id
+            next_id += 1            
+
+
+    for inst in new_DEM:
+
+        if inst.type == "error":
+
+            new_targets = []
+
+            for t in inst.targets_copy():
+                if t.is_relative_detector_id():
+                    new_targets.append(
+                        stim.target_relative_detector_id(mapping[t.val])
+                    )
+                else:
+                    new_targets.append(t)
+
+            final_DEM.append(
+                "error",
+                targets=new_targets,
+                parens_arguments=inst.args_copy()[0]
+            )
+
+        elif inst.type == "detector":
+
+            old = inst.targets_copy()[0].val
+
+            final_DEM.append(
+                "detector",
+                targets=[stim.target_relative_detector_id(mapping[old])],
+                parens_arguments=inst.args_copy()
+            )
+
+        else:
+            final_DEM.append(inst)
+
+    return final_DEM

f85fa844ed9e6963d83e65c81c74f3bf812603c6
acatspirit <acatspirit@gmail.com>
2026-08-06 15:38:16 -0400

    get rid of mp parts to run on dcc

--- simulation_scripts/ler_for_decoder_switching.py
+++ simulation_scripts/ler_for_decoder_switching.py
@@ -22,7 +22,8 @@ import pickle
 Adjust for UF
 '''
 
-def get_cutoffs_for_input_switch_rate(target_switch_rate,plot=False, weak_decoder='bplsd', num_shots=100_000):
+def get_cutoffs_for_input_switch_rate(target_switch_rate,plot=False, weak_decoder='bplsd', num_shots=100_000, folder_name = sys.path[-1] + f'/data/cluster_norm_statistics/'):
+
 
     code_names = ["[[72,12,6]]", "[[90,8,10]]", "[[126,8,10]]", "[[144,12,12]]", "[[162,8,14]]"] 
     cutoffs_to_set = {}
@@ -34,7 +35,7 @@ def get_cutoffs_for_input_switch_rate(target_switch_rate,plot=False, weak_decode
     cnt=0
     for code_name in code_names:
     
-        txt_to_load = sys.path[-1] + f'/saved_data/cluster_norm_statistics/cluster_norm_distributions_code_{code_name}_{weak_decoder}_max_shots_{num_shots}.txt'
+        txt_to_load = folder_name + f'cluster_norm_distributions_code_{code_name}_{weak_decoder}_max_shots_{num_shots}.txt'
 
         with open(txt_to_load, "rb") as file:
             data = pickle.load(file)
@@ -318,70 +319,56 @@ def get_ler_for_decoder_switching_dcc(target_switch_rate, num_shots, shots_per_j
     '''
 
 
-    #ADJUST THIS FOR UF
     cutoffs_to_set,_ = get_cutoffs_for_input_switch_rate(target_switch_rate=target_switch_rate) 
 
-    colors = ["tab:blue","tab:orange","tab:green","tab:red","tab:purple"]
+    # colors = ["tab:blue","tab:orange","tab:green","tab:red","tab:purple"]
+    task_id = int(os.environ.get("SLURM_ARRAY_TASK_ID"),0)
 
     basis      = 'Z'
     code_names = ["[[72,12,6]]", "[[90,8,10]]", "[[126,8,10]]", "[[144,12,12]]", "[[162,8,14]]"]    
-    ps         = [2e-3,3e-3,4e-3,5e-3] #I RUN THESE RATES ONLY FOR BPLSD
+    # ps         = [2e-3,3e-3,4e-3,5e-3] #I RUN THESE RATES ONLY FOR BPLSD
+    ps = np.logspace(-4,-2.5,6)
     num_rounds = 25
     
-
-    def process_one_round_value(code_name,p,num_shots,cutoff):
-        
-        print("Code_name,rds,p,shots:",(code_name,num_rounds,p,num_shots))
-
-        n, k, d = map(int, code_name.strip("[]").split(","))
-        
-
-        nbuffer = d            #Buffer region
-        F       = d//2         #Commit region
-        W       = nbuffer + F  #Entire window
-
-        test  = decoder_switching_class(code_name=code_name,
-                                            num_rounds=num_rounds,
-                                            p=p,
-                                            basis=basis,
-                                            num_shots=num_shots,
-                                            W=W,
-                                            F=F,
-                                            strong_decoder_option=strong_decoder,
-                                            weak_decoder_option=weak_decoder)    
-        
-        new_shots,cluster_norms,switch_times,logical_errors = test.decode_with_sliding_window_and_decoder_switching(cluster_norm_cutoff=cutoff)
-
-
-        num_windows =len(test.weak_decoder) #total # of windows -- needed for getting switch rates
-
-        
-        result = {"logical_errors": np.sum(logical_errors),"cluster_norms": cluster_norms,
-                    "switch_times": np.sum(switch_times), "num_windows": num_windows}
-        
-
-        print("Sim done.")
-
-        return code_name,p,new_shots,result
-    
-    
-
     tasks = []
-    import multiprocessing as mp
-    n_jobs = mp.cpu_count()    
-    chunk_size = max(shots_per_job, num_shots // (100 * n_jobs)) 
 
     for code_name in code_names:
-
         for p in ps:
+            for _ in range(num_shots // shots_per_job):
+                tasks.append((code_name,p,shots_per_job))
+                
+    if task_id >= len(tasks):
+        print(f"Task ID {task_id} is out of bounds for {len(tasks)} tasks. Exiting cleanly.")
+        return
 
-            tasks.extend(
-                (code_name,p,chunk_size)
-                for _ in range(num_shots // chunk_size) )       
+    code_name, p, shots = tasks[task_id]
+    cutoff = cutoffs_to_set[(code_name, p)]
 
+    print(f"--- RUNNING ARRAY TASK {task_id} ---")
+    print(f"Code: {code_name}, p: {p}, shots: {shots}")
 
-    results = Parallel(n_jobs=-1,verbose=50,)(delayed(process_one_round_value)(code_name,p,shots,cutoffs_to_set[(code_name,p)]) for code_name, p,shots in tasks)      
+    n, k, d = map(int, code_name.strip("[]").split(","))
+            
     
+    nbuffer = d            #Buffer region
+    F       = d//2         #Commit region
+    W       = nbuffer + F  #Entire window
+
+    test  = decoder_switching_class(code_name=code_name,
+                                        num_rounds=num_rounds,
+                                        p=p,
+                                        basis=basis,
+                                        num_shots=shots,
+                                        W=W,
+                                        F=F,
+                                        strong_decoder_option=strong_decoder,
+                                        weak_decoder_option=weak_decoder)    
+    
+    new_shots,cluster_norms,switch_times,logical_errors = test.decode_with_sliding_window_and_decoder_switching(cluster_norm_cutoff=cutoff)
+
+
+    num_windows =len(test.weak_decoder) #total # of windows -- needed for getting switch rates
+
     total_errors  = {}
     total_shots   = {}
     total_switch_times = {}
@@ -416,15 +403,9 @@ def get_ler_for_decoder_switching_dcc(target_switch_rate, num_shots, shots_per_j
                         }      
     
 
-    fig, ax = plt.subplots(1,2)
-
     eps_to_save = {}
     errs_in_eps_to_save ={}
-
-
-    #================= Plot ler from decoder switching ===============================================
     
-    cnt=0
     for code_name in code_names:
 
         n, k, d = map(int, code_name.strip("[]").split(","))
@@ -437,21 +418,9 @@ def get_ler_for_decoder_switching_dcc(target_switch_rate, num_shots, shots_per_j
         eps_errs = { p: (pL_errs[p] / num_rounds) * (1 - pL_vals[p])**(1 / num_rounds - 1)
                     for p in ps }        
 
-        ax[0].errorbar(ps,eps.values(),yerr=eps_errs.values(),label=f"{code_name}, {weak_decoder} to {strong_decoder}",color=colors[cnt],marker='o',markeredgecolor='k')
-        cnt+=1
-
         eps_to_save[code_name] = eps
         errs_in_eps_to_save[code_name] = eps_errs 
 
-        
-
-    ax[0].set_yscale('log')
-    ax[0].set_xscale('log')
-    ax[0].legend(fontsize=9)
-    ax[0].set_xlabel("physical error rate")
-    ax[0].set_ylabel("LER per SEC")
-
-    
 
     switch_rates = {
         key: total_switch_times[key] / total_windows[key]
@@ -465,34 +434,6 @@ def get_ler_for_decoder_switching_dcc(target_switch_rate, num_shots, shots_per_j
         for key in switch_rates
     }    
 
-    cnt=0
-
-    #================= Plot the switch rate ===============================================
-
-    for code_name in code_names:
-        # all_switch_rates = {p: total_switch_times[(code_name,p)] for p in ps}
-        y = np.array([ switch_rates[(code_name, p)] for p in ps ])
-
-        yerr = np.array([ switch_yerr[(code_name, p)] for p in ps ])
-
-        
-        ax[1].errorbar(ps,y,yerr=yerr, marker='o',color=colors[cnt],label=f"{code_name}")   
-        cnt+=1 
-
-    
-
-    ax[1].set_ylabel("$p_{switch}$")
-    ax[1].set_xlabel("physical error rate")
-    ax[1].set_title(f"for $p_s$: {target_switch_rate}")
-    ax[1].set_yscale('log')
-    ax[1].set_xscale('log')
-    ax[1].legend(fontsize=9)
-
-
-    
-
-    plt.tight_layout()
-    plt.show()
 
     dict_to_save = {"basis":basis,
                     "weak_decoder": weak_decoder,
@@ -513,7 +454,7 @@ def get_ler_for_decoder_switching_dcc(target_switch_rate, num_shots, shots_per_j
                     "total_windows": total_windows}         
     
 
-    txt_to_save = sys.path[-1] + f'/saved_data/decoder_switching_data/decoder_switching_target_ps_{target_switch_rate}_weak_{weak_decoder}_strong_{strong_decoder}_max_shots_{num_shots}.txt' #p_2e_minus_3_Gross_only
+    txt_to_save = sys.path[-1] + f'/data/decoder_switching_data/decoder_switching_target_ps_{target_switch_rate}_weak_{weak_decoder}_strong_{strong_decoder}_max_shots_{num_shots}.txt' #p_2e_minus_3_Gross_only
 
     with open(txt_to_save, 'w') as file:
         file.write(str(dict_to_save))      

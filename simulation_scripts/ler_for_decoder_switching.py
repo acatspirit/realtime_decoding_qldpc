8510478aae03e5abf2cd533c8e95f5433165a612
acatspirit <acatspirit@gmail.com>
2026-08-11 11:57:59 -0400

    taking 5e-3 switch rate data for uf->tesserat 1e6 shots max

--- simulation_scripts/ler_for_decoder_switching.py
+++ simulation_scripts/ler_for_decoder_switching.py
@@ -680,21 +680,27 @@ def plot_decoder_switching_results(target_switch_rate, weak_decoder, strong_deco
 
 
 if __name__ == "__main__":
+    num_shots = 1_000_000
+    shots_per_job = 50_000
+    target_switch_rate = 5e-3
+    weak_decoder = 'uf'
+    strong_decoder = 'tesseract'
+
     # to run on the cluster
-    get_ler_for_decoder_switching_dcc(num_shots=1_000_000, shots_per_job=50_000, target_switch_rate=1e-3, weak_decoder='uf', strong_decoder='tesseract')
+    get_ler_for_decoder_switching_dcc(num_shots=num_shots, shots_per_job=shots_per_job, target_switch_rate=target_switch_rate, weak_decoder=weak_decoder, strong_decoder=strong_decoder)
 
     # run this once you have stuff from the cluster
     # merge_dcc_results(
-    #     target_switch_rate=0.25, # Update with the switch rate you ran
-    #     weak_decoder='uf', 
-    #     strong_decoder='tesseract', 
-    #     num_shots_max=100_000     # Update to your actual num_shots
+    #     target_switch_rate=target_switch_rate, # Update with the switch rate you ran
+    #     weak_decoder=weak_decoder,
+    #     strong_decoder=strong_decoder,
+    #     num_shots_max=num_shots     # Update to your actual num_shots
     # )
 
     # run this to plot the results from decoder switching
-    # plot_decoder_switching_results(
-    #     target_switch_rate=0.25, # Update with the switch rate you ran
-    #     weak_decoder='uf',
-    #     strong_decoder='tesseract',
-    #     num_shots_max=100_000     # Update to your actual num_shots
-    # )
+    plot_decoder_switching_results(
+        target_switch_rate=target_switch_rate, # Update with the switch rate you ran
+        weak_decoder=weak_decoder,
+        strong_decoder=strong_decoder,
+        num_shots_max=num_shots     # Update to your actual num_shots
+    )

1938146f374dc6bf561bf9527f2cb05063150f21
acatspirit <acatspirit@gmail.com>
2026-08-21 16:29:24 -0400

    tesseract 1e-3, many more shots

--- simulation_scripts/ler_for_decoder_switching.py
+++ simulation_scripts/ler_for_decoder_switching.py
@@ -782,11 +782,11 @@ def plot_decoder_switching_results(target_switch_rate, weak_decoder, strong_deco
 
 
 if __name__ == "__main__":
-    num_shots = 1_000_000
-    shots_per_job = 50_000
+    num_shots = 10_000_000
+    shots_per_job = 500_000
     target_switch_rate = 1e-3
     weak_decoder = 'uf'
-    strong_decoder = 'relay_bp'
+    strong_decoder = 'tesseract'
 
     # to run on the cluster / get data on cluster
     get_ler_for_decoder_switching_dcc(num_shots=num_shots, shots_per_job=shots_per_job, target_switch_rate=target_switch_rate, weak_decoder=weak_decoder, strong_decoder=strong_decoder)
@@ -799,7 +799,7 @@ if __name__ == "__main__":
     #     num_shots_max=num_shots     # Update to your actual num_shots
     # )
 
-    # # run this to plot the results from decoder switching
+    # run this to plot the results from decoder switching
     # plot_decoder_switching_results(
     #     target_switch_rate=target_switch_rate, # Update with the switch rate you ran
     #     weak_decoder=weak_decoder,

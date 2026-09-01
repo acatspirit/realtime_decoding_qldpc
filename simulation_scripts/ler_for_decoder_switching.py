93107562cb34fecb690e9529a7b7683830f79ef1
acatspirit <acatspirit@gmail.com>
2026-09-01 11:05:19 -0400

    No LER, trying 10^8 shots with tesseract

--- simulation_scripts/ler_for_decoder_switching.py
+++ simulation_scripts/ler_for_decoder_switching.py
@@ -985,11 +985,11 @@ def plot_switching_gains_vs_switch_rate(weak_decoder, strong_decoder, p_physical
 
 
 if __name__ == "__main__":
-    num_shots = 1_000_000
-    shots_per_job = 50_000
+    num_shots = 100_000_000
+    shots_per_job = 5_000_000
     target_switch_rate = 5e-1
     weak_decoder = 'uf'
-    strong_decoder = 'relay_bp' # change back to tesseract
+    strong_decoder = 'tesseract' # change back to tesseract
 
     # to run on the cluster / get data on cluster
     get_ler_for_decoder_switching_dcc(num_shots=num_shots, shots_per_job=shots_per_job, target_switch_rate=target_switch_rate, weak_decoder=weak_decoder, strong_decoder=strong_decoder)

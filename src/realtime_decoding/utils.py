d3e00e21953c55d05e65dddb558a42ce92e4191b
acatspirit <acatspirit@gmail.com>
2026-09-08 16:48:48 -0400

    add the erasure tracking vector to utils

--- src/realtime_decoding/utils.py
+++ src/realtime_decoding/utils.py
@@ -68,9 +68,21 @@ def get_window_dems(window_check_set, window_observable_set, window_priors_set):
     return window_dems_set
 
 
-def get_erased_mechanisms_from_DEM():
-    """ Given a DEM, get the error mechanisms where erasures occurred
+def get_erased_mechanisms_from_DEM(dem:stim.DetectorErrorModel, p_cutoff = 0.02):
+    """ Given a DEM, get the error mechanisms where erasures occurred. This is set by a cutoff, since erasures
+        produce an error mechanism with p=0.5. General rule - cutoff should be p_max error?
     """ 
     # get all the DEM entries over some prob p that are the erasures, set all others to 0. This is the erasures vec
+    # Q - does the num_detectors set the shape of the H_DEM or do the actual error mechanisms?
+    erased_errors = np.zeros(shape=(dem.num_errors,)).flatten()
+    i = 0
 
-    return
+    for inst in dem:
+        if inst.type == "error":
+            if inst.args_copy()[0] > p_cutoff:
+                erased_errors[i] = 1
+            else:
+                erased_errors[i] = 0
+        i+= 1
+
+    return erased_errors
